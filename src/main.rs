92d79fa0ba98660e68a7659196986e6fbb96a943
Gilbert Röhrbein <gilbert@ifsr.de>
2020-12-04 18:14:48 +0100

    day two

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use regex::{self, Regex};
 use std::fs;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     day1_1()?;
     day1_2()?;
+    day2()?;
     Ok(())
 }
 
@@ -34,7 +36,7 @@ fn day1_2() -> std::io::Result<()> {
                     return Some(a * b * c);
                 }
             }
-        };
+        }
         None
     };
     let hit = find_hit().expect("no hit found");
@@ -42,3 +44,23 @@ fn day1_2() -> std::io::Result<()> {
     Ok(())
 }
 
+fn day2() -> Result<(), Box<dyn std::error::Error>> {
+    let filepath = "src/passwords.input";
+    let regex = Regex::new(r"(\d+)-(\d+) ([a-z]): (\w+)")?;
+    let hits = fs::read_to_string(&filepath)?
+        .split('\n')
+        .filter(|s| {
+            if let Some(caps) = regex.captures(s) {
+                let min: usize = caps.get(1).unwrap().as_str().parse().unwrap();
+                let max: usize = caps.get(2).unwrap().as_str().parse().unwrap();
+                let letter = caps.get(3).unwrap().as_str().chars().nth(0).unwrap();
+                let password = caps.get(4).unwrap().as_str();
+                let char_count = password.chars().filter(|&c| letter == c).count();
+                return min <= char_count && char_count <= max;
+            }
+            false
+        })
+        .count();
+    println!("day2: {}", hits);
+    Ok(())
+}
